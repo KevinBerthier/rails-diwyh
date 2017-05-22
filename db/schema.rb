ad5b88d017f2caa579f420365bbd058e505ed2b1
Kevin Berthier <kevinberthier@orange.fr>
2017-05-22 11:05:17 +0200

    workshops slugged

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519154803) do
+ActiveRecord::Schema.define(version: 20170522085952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,7 +152,9 @@ ActiveRecord::Schema.define(version: 20170519154803) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
+    t.string "slug"
     t.index ["craftman_id"], name: "index_workshops_on_craftman_id"
+    t.index ["slug"], name: "index_workshops_on_slug", unique: true
   end
 
   add_foreign_key "bookings", "users"
