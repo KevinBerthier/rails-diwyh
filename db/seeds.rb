9ca74e171add8f6647f054b1623bd42a9e6b7c41
loic cessot <loic.cessot@gadz.org>
2017-05-31 12:13:56 +0200

    content done

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,7 +58,8 @@ wood = Universe.new({
           chapters in the trees whose narratives will
           be reconstituted a century later.”",
   third_description: "Since prehistoric times, wood has allowed the man to make fire, to build shelters or to make tools and weapons. Some of the most important inventions of the civilized world - the wheel, the printing press and the first computer, the abacus - were first made of wood. Nowadays, the uses of this material remain as varied as in the past.",
-  fourth_description: "The more one knows about the unique characteristics of wood and its source, the better one can understand the degree of warmth and beauty that it brings to our everyday décor. Furniture made of wood is one of the few things in the world that all people can own and know that they are the only person in the world who owns that particular grain pattern and its inherent beauty. Each grain pattern is a unique masterpiece of design, texture and splendor. Even what some may view as a defect, like a knot or other natural blemishes, can add more beauty and character to any given piece of furniture.",
+  fourth_description: "The more one knows about the unique characteristics of wood and its source, the better one can understand the degree of warmth and beauty that it brings to our everyday décor.Each grain pattern is a unique masterpiece of design, texture and splendor. Even what some may view as a defect, like a knot or other natural blemishes, can add more beauty and character to any given piece of furniture.",
+
   banner: open_photo("wood_univers.jpg"),
   banner_bottom: open_photo("copeaux.jpg"),
   collage: open_photo("background_wood.png"),
@@ -120,7 +121,7 @@ sarah = Craftman.new(
     first_name: "Sarah",
     last_name: "Carrucan",
     phone: "61597946943",
-    description: "Wood and life are inextricably connected, and wood is literally life to us. Therefore, we use solid wood from renewable resources in our production, only environmentally-friendly materials for treatment, natural oils and wax that emphasize the luxurious and organic beauty of wood texture and that enable it to 'breathe' naturally.",
+    description: "Wood and life are inextricably connected, and wood is literally life to us. Therefore, we use solid wood from renewable resources in our production to reveal the oganic beauty of wood texture and that enable it to 'breathe' naturally.",
     second_description: "This emphasizes the natural colour diversity, while the use of harmful chemical substances for wood treatment is avoided by using vinyl-based glue.",
     address: "Warrnambool, VIC, 3280 Australia",
     locality: "Warrnambool",
@@ -137,7 +138,7 @@ andrew = Craftman.new(
     last_name: "Wainwright",
     phone: "(519) 575-9043",
     description: "The fundamental is my need to express myself using both my mind and my hands. For me, my work is as much about the process itself, whether it be designing, or solving technical challenges to realize my vision, as it is about the finished piece.",
-    second_description: "As a woodworker, my intent is to convey to the viewer an awareness of the powerful synergies that develop between form and the inherently sensual nature of wood.  I have a deep understanding of and respect for the properties of wood, and this informs my choice of technique:  I allow for the seasonal movement of wood and use time-tested, traditional joinery.  I build with the intent that  a piece should last over generations, and, in so doing, confront the values of the disposable society in which we now live.",
+    second_description: "As a woodworker, my intent is to convey to the viewer an awareness of the powerful synergies that develop between form and the inherently sensual nature of wood.",
     address: "44 Otonabee Dr Unit 3, Kitchener, ON N2C 1L6, Canada",
     locality: "Kitchener",
     birth_date: "03/08/1956",
@@ -199,15 +200,16 @@ puts '6 craftmen created...'
 
 workshop_attributes = [
   {
-    title:        "Your scandi style wooden table in 3 days",
-    price:        500000,
+    title:        "Your scandi style wooden table in 2 days",
+    price:        2000,
     description:  "Curved lines at the corners of the dining table soften the silhouette, while the angled legs add a striking edge to the design.Crafted from robust solid oak legs, this table and bench set is made for everyday use. In a lacquer finish to protect it from wear and tear.",
     craftman:     laura,
     artisan_pic:  open_photo("wood_workshop.jpg"),
     skills:       "Understand How Wood Works and Behaves, Use a Hand Plane, Cut a Mortise and Tenon Joint, master sharpen saws, Prep Lumber With Hand Tools, Cut a Mortise and Tenon Joint, surface finishing ."
+  },
   {
-    title:        "Create your own rocking chair",
-    price:        180000,
+    title:        "Create your own rocking chair in tropical wood",
+    price:        3000,
     description:  "A hand carved seat and well balanced rockers make this a very comfortable chair. The rockers are constructed by way of lamination - timber is cut into thin strips and then glued together in the desired shape. This method provides exceptional strength. Joins are mortice and tenoned and the main joins are also dowelled for extra strength. The chair is handed sanded and finished with an oil and wax finish.",
     craftman:     sarah,
     artisan_pic:  open_photo("wood_workshop.jpg"),
@@ -215,15 +217,15 @@ workshop_attributes = [
   },
   {
     title:        "Create some wooden plays for your kid",
-    price:        60000,
+    price:        200,
     description:  "We are going to make possible your children to play with their favorite football team members made in the purest wood from Jura. With a striker and a goal keeper, marbles are being turned into ballon d'or! Each box includes 2 wooden players, 1 wooden goal, 3 colored marbles according to the team and 3 stickers to custom the wooden pieces.",
     craftman:     guillaume,
     artisan_pic:  open_photo("wood_workshop.jpg"),
     skills:       "Understand How Wood Works and Behaves, Use Sharpen Saws, Furniture finishing."
   },
   {
-    title:        "Create your dream design desk",
-    price:        40000,
+    title:        "Your dream design desk in walnut made in 3 days",
+    price:        3000,
     description:  "This is one of the best desk designs when it comes to storage space. This well-designed Desk holds stacks of paper and files in a neat compartment under the desk, helping you clear all the clutter on the desks` surface. Even if underneath the desk there are files and folders, this is not an impediment for a comfortable seating experience.",
     craftman:     guillaume,
     artisan_pic:  open_photo("wood_workshop.jpg"),
@@ -231,7 +233,7 @@ workshop_attributes = [
   },
   {
     title:        "Bring to life a design bench for your garden",
-    price:        40000,
+    price:        2000,
     description:  "This simple bench or banquette is a stunner with it’s clean lines and slightly coastal feel! This is the perfect little seating piece for outside, perhaps with a dining table or even configured with a corner unit into a nook like arrangement. That would look equally as fabulous inside or out!",
     craftman:     andrew,
     artisan_pic:  open_photo("wood_workshop.jpg"),
@@ -239,7 +241,7 @@ workshop_attributes = [
   },
   {
     title:        "Wood carving for beginners: how to get started !",
-    price:        5000,
+    price:        500,
     description:  "Wood carving is a fun, accessible, and creative craft that doesn’t require a fancy studio, or equipment. All you need is a piece of wood and something sharp to carve it with. Of course, what kind of wood and what kind of carving tools you pick will affect the difficulty, safety, and result of your wood carving endeavors. In this wood carving for beginners guide, we’ll go over some of the most important information a new craftsman or craftswoman needs to know to get started.",
     craftman:     guillaume,
     artisan_pic:  open_photo("wood_workshop.jpg"),
@@ -247,7 +249,7 @@ workshop_attributes = [
   },
   {
     title:        "Minimalist framing for family pictures",
-    price:        2000,
+    price:        300,
     description:  "Many collectors and artists have an eye for selecting the right frame and can make sound decisions with little guidance from a professional. Don’t underestimate the importance of framing your pictures too in the most suitable and visually attractive way. We will learn how to make the right selections through minimalist examples, and your pictures will bring pleasure for generations.",
     craftman:     ernest,
     artisan_pic:  open_photo("wood_workshop.jpg"),
@@ -255,7 +257,7 @@ workshop_attributes = [
   },
   {
     title:        "learn how to frame your paintings with taste",
-    price:        4000,
+    price:        600,
     description:  "Like the setting for a diamond, the frame around a work of art is the finishing touch, the element that completes and elevates a painting, presenting it to the viewer in its best possible light. Framing, however, is an art in and of itself, and just as a good frame choice can greatly enhance the appearance of a work, a poor frame choice can drastically diminish a work.",
     craftman:     thomas,
     artisan_pic:  open_photo("wood_workshop.jpg"),
